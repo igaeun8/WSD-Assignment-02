d2c7b7c6e1e83793cbf22e8c352b32fa5a6ebc5c
leegaeun <igaeun8@gmail.com>
2024-11-08 22:52:34 +0900

    교수님 데모 페이지로 배포 연습

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'WSD-Assignment-02' title`, () => {
+  it(`should have the 'Angular-demo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('WSD-Assignment-02');
+    expect(app.title).toEqual('Angular-demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, WSD-Assignment-02');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-demo');
   });
 });
